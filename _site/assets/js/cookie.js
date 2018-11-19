0372b085677e26fccdc72cad18f26c0144335801
root <root@localhost.localdomain>
2018-11-19 17:14:15 +0000

    cookie consent

---
+++ _site/assets/js/cookie.js
@@ -0,0 +1,11 @@
+if (typeof(cookie) === 'undefined') {
+
+$(document).ready(function(){
+    setTimeout(function () {
+        $("#cookieConsent").fadeIn(200);
+     }, 4000);
+    $("#closeCookieConsent, .cookieConsentOK").click(function() {
+        $("#cookieConsent").fadeOut(200);
+    });
+});
+};
