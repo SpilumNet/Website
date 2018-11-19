9b4f04946dc4068d661c3409245f740f3f1b25b1
root <root@localhost.localdomain>
2018-11-19 18:37:28 +0000

    cookie consent

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -1,11 +1,11 @@
-if (typeof(cookie) === 'undefined') {
-
+if (document.cookie === false){
 $(document).ready(function(){
     setTimeout(function () {
         $("#cookieConsent").fadeIn(200);
      }, 4000);
     $("#closeCookieConsent, .cookieConsentOK").click(function() {
         $("#cookieConsent").fadeOut(200);
+        document.cookie = "yes";
     });
 });
-};
+}
