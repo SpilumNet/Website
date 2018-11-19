26c7e3d138b435d4691960686d8d4e53112a96ec
Apher <apher@localhost.localdomain>
2018-11-19 18:56:24 +0000

    Cookie reading function

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -1,11 +1,29 @@
-if (document.cookie === ""){
+function getCookie(name) {
+  var dc = document.cookie;
+  var prefix = name + "=";
+  var begin = dc.indexOf("; " + prefix);
+  if (begin == -1) {
+    begin = dc.indexOf(prefix);
+    if (begin != 0) return null;
+  }
+  else {
+    begin += 2;
+    var end = document.cookie.indexOf(";", begin);
+    if (end == -1) {
+      end = dc.length;
+    }
+  }
+  return decodeURI(dc.substring(begin + prefix.length, end));
+}
+
+if (getCookie("consent") === null){
 $(document).ready(function(){
     setTimeout(function () {
         $("#cookieConsent").fadeIn(200);
      }, 4000);
     $("#closeCookieConsent, .cookieConsentOK").click(function() {
         $("#cookieConsent").fadeOut(200);
-        document.cookie = "yes";
+        document.cookie = "consent=yes;";
     });
 });
 }
