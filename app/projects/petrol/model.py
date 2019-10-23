312376712ea545b809a50dff81a82064d45ac6fb
Tristan Andersen <tristanpn02@gmail.com>
2019-10-23 16:57:17 +0000

    Complete petrol project

--- app/projects/petrol/model.py
+++ app/projects/petrol/model.py
@@ -5,5 +5,4 @@ import urllib.request
 def get_data():
     with urllib.request.urlopen("https://apis.is/petrol") as url:
         data = json.loads(url.read().decode())
-        print(data['results'])
         return data['results']
--- app/projects/petrol/petrol.py
+++ app/projects/petrol/petrol.py
@@ -21,6 +21,32 @@ def index():
             companies[minBensin['company']]['diesel'] = minBensin['diesel']
         obj.remove(minBensin)
 
-    print(companies)
+    mincompany = min((int(x['bensin95']), int(x['diesel'])) for x in companies.values())
+    mincompany = [x for x in companies if (int(companies[x]['bensin95']), int(companies[x]['diesel'])) == mincompany]
 
-    return render_template('projects/petrol/index.html', company_list=companies, title='Petrol')
+    return render_template('projects/petrol/index.html', company_list=companies, title='Petrol', mincompany=mincompany)
+
+
+@bp.route('/projects/petrol/<id>')
+def company(id):
+    obj = model.get_data()
+    companies = {}
+    comp = {}
+
+    for i in obj:
+        if i['company'] not in companies:
+            companies[i['company']] = {'bensin95': i['bensin95'], 'diesel': i['diesel'], 'locations': []}
+
+    while obj:
+        minBensin = min(obj, key=lambda x: x['bensin95'])
+        if minBensin['bensin95'] < companies[minBensin['company']]['bensin95']:
+            companies[minBensin['company']]['bensin95'] = minBensin['bensin95']
+        if minBensin['diesel'] < companies[minBensin['company']]['diesel']:
+            companies[minBensin['company']]['diesel'] = minBensin['diesel']
+        companies[minBensin['company']]['locations'].append(minBensin['name'])
+        obj.remove(minBensin)
+
+    for obj, value in companies.items():
+        if str(obj) == id:
+            comp[obj] = companies[obj]
+            return render_template('projects/petrol/company.html', company=comp)
